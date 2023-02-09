e2c54e937e7a964bcd6956b0c62f836fc76f27a1
Zhenxu Wang <mason123wp@gmail.com>
2023-02-08 22:51:23 -0500

    added suppresswarning

--- test/unitTesting/LedgerItemTester.java
+++ test/unitTesting/LedgerItemTester.java
@@ -2,6 +2,8 @@ package unitTesting;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,11 +24,32 @@ class LedgerItemTester {
 		assertTrue(LedgerItem.getInstanceOf(input) instanceof LedgerItem, "static factory method failed");
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Test 
 	void testSetterGetter() {
 		assertEquals("", obj.getNote());
 		assertTrue(Math.abs(obj.getAmount() - 0.00) <= 0.0001);
 		assertEquals("", obj.getItemName());
 		assertEquals(null, obj.getDate());
+		
+		double amount = 15.63;
+		String s = "Wendy's";
+		String n = "double combo, with fries and coke";
+		Date d = new Date();
+		d.setYear(2023);
+		d.setMonth(0);
+		d.setDate(10);
+		d.setHours(11);
+		d.setMinutes(30);
+		d.setSeconds(0);
+		obj.setDate(d);
+		obj.setAmount(amount);
+		obj.setItemName(s);
+		obj.setNote(n);
+		
+		assertEquals(n, obj.getNote());
+		assertTrue(Math.abs(obj.getAmount() - amount) <= 0.0001);
+		assertEquals(s, obj.getItemName());
+		assertEquals(d, obj.getDate());
 	}
 }
