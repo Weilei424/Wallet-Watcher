526250108cf6d9868ac90f725a02d52efe22b76d
Zhenxu Wang <mason123wp@gmail.com>
2023-02-08 22:46:30 -0500

    added exception package
    added InvalidDateException with implementation

---
+++ src/exceptions/InvalidDateException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+@SuppressWarnings("serial")
+public class InvalidDateException extends IllegalArgumentException {
+
+	public InvalidDateException() {
+		super();
+	}
+	
+	public InvalidDateException(String msg) {
+		super(msg);
+	}
+}
