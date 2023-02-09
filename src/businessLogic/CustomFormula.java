2a26d019e9f9a3f934067f0002a1b07a0508cf25
Zhenxu Wang <mason123wp@gmail.com>
2023-02-08 21:14:09 -0500

    Created businessLogic package. Added CustomFormula interface, and MathUtil class to businessLogic packge.
    Created persistence package.
    Added LedgerItem class to persistence package.
    
    Implemented constructors, getters, setters, and static factory method
    for LedgerItem.

---
+++ src/businessLogic/CustomFormula.java
@@ -0,0 +1,5 @@
+package businessLogic;
+
+public interface CustomFormula {
+
+}
---
+++ src/persistence/LedgerItem.java
@@ -0,0 +1,71 @@
+package persistence;
+
+import java.util.Date;
+
+public class LedgerItem {
+	Date date;
+	double amount;
+	String itemName;
+	String note;
+	
+	private LedgerItem(Date date, double amount, String itemName, String note) {
+		this.date = date;
+		this.amount = amount;
+		this.itemName = itemName;
+		this.note = note;
+	}
+	
+	/**
+	 * Static factory method to initialize LedgerItem object
+	 * @param	input string from user
+	 * @return	instance is an object with attributes processed by Tokenizer
+	 */
+	public static LedgerItem getInstanceOf(String input) {
+		try {
+//			Tokenizer static method to check if the input is legit.
+//			Tokenizer.check(input);
+		} catch (Exception e) {
+			System.out.println("");
+		}
+		
+//		Date date = Tokenizer.getDate();
+//		double amount = Tokenizer.getAmount();
+//		String itemName = Tokenizer.getItemName();
+//		String note = Tokenizer.getNote();
+		LedgerItem obj = new LedgerItem(null, 0.00, "", "");
+		return obj;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+}
