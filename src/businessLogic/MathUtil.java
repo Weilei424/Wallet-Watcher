cd521cd4b6fc2f3570c2cc90a2cfac6723de97dd
Zhenxu Wang <mason123wp@gmail.com>
2023-02-08 22:43:24 -0500

    implemented calcMonth(), calcYear(), calcBiweek()

--- src/businessLogic/MathUtil.java
+++ src/businessLogic/MathUtil.java
@@ -1,5 +1,86 @@
 package businessLogic;
 
-public class MathUtil {
+import java.util.Date;
 
+import exceptions.InvalidDateException;
+
+public final class MathUtil {
+	
+	/**
+	 * private constructor prevent from creation of object
+	 */
+	private MathUtil() {}
+	
+	/**
+	 * This method calculates the number of month difference of input dates.
+	 * @param beginAt	is a Date object that is the beginning date.
+	 * @param endAt		is a Date object that is the ending date.
+	 * @return			integer number of month difference.
+	 */
+	@SuppressWarnings("deprecation")
+	public static int calcMonth(Date beginAt, Date endAt) {
+		try {
+			boolean flag = beginAt.getTime() - endAt.getTime() >= 0;
+			if (flag)
+				throw new InvalidDateException();
+		} catch (IllegalArgumentException e) {
+			System.out.println("invalid date");
+		}
+		int year = endAt.getYear() - beginAt.getYear();
+		int month = endAt.getMonth() - beginAt.getMonth();
+		
+		return 12 * year + month;
+	}
+	
+	/**
+	 * This method calculates the number of years difference of input dates.
+	 * @param beginAt	is a Date object that is the beginning date.
+	 * @param endAt		is a Date object that is the ending date.
+	 * @return			integer number of years difference.
+	 */
+	@SuppressWarnings("deprecation")
+	public static int calcYear(Date beginAt, Date endAt) {
+		try {
+			boolean flag = beginAt.getYear() - endAt.getYear() >= 0;
+			if (flag)
+				throw new InvalidDateException();
+		} catch (IllegalArgumentException e) {
+			System.out.println("invalid date");
+		}
+		int year = endAt.getYear() - beginAt.getYear();
+		
+		return year;
+	}
+	
+	/**
+	 * This method calculates the number of biweeks difference of input dates.
+	 * @param beginAt	is a Date object that is the beginning date.
+	 * @param endAt		is a Date object that is the ending date.
+	 * @return			integer number of years difference.
+	 */
+	@SuppressWarnings("deprecation")
+	public static int calcBiweek(Date beginAt, Date endAt) {
+		try {
+			boolean flag = beginAt.getYear() - endAt.getYear() >= 0;
+			if (flag)
+				throw new InvalidDateException();
+		} catch (IllegalArgumentException e) {
+			System.out.println("invalid date");
+		}
+		int biweeks = (MathUtil.getWeek(endAt) -MathUtil.getWeek(endAt)) / 2;
+		
+		return biweeks;
+	}
+	
+	/**
+	 * Returns the number of week since January 1, 1970, 00:00:00 GMTrepresented by this Date .
+	 * @param date	is the current date.
+	 * @return the number of weeks.
+	 */
+	private static int getWeek(Date date) {
+		final double CONVENTION = 6.048e+8;
+		int week = (int) (date.getTime() / CONVENTION);
+		
+		return week;
+	}
 }
