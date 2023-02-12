c017bbb5b20b5a6b5952a2bb96abce52f80658ee
manstein <manstein@my.yorku.ca>
2023-02-12 08:42:02 -0500

    "Add get sumincome and sumofmonth method"

--- src/persistence/Income.java
+++ src/persistence/Income.java
@@ -1,6 +1,7 @@
 package persistence;
 
 import java.util.Date;
+import java.util.List;
 
 public class Income {
 	LedgerItem info;
@@ -8,12 +9,46 @@ public class Income {
 	public Income(LedgerItem info) {
 		this.info=info;
 	}
+	
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public Date getDate() {
 		return this.info.getDate();
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public double getAmount() {
 		return this.info.getAmount();
 	}
+	/**
+	 * 
+	 * @param list
+	 * @return
+	 */
+	public static double getSumIncome(List<Income>list) {
+		double amount =0;
+		for(Income INCOME:list)
+			amount+=INCOME.getAmount();
+		return amount;
+			
+			
+	}
+	@SuppressWarnings("deprecation")
+	public static double getSumMOnth(int month,List<Income>list) {
+		double amount =0;
+		for (Income income:list)
+			if(income.info.getDate().getMonth()==month)
+				amount+=income.info.getAmount();
+		return amount;
+	}
+	
+	
+	
 	
 }
