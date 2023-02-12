1a5444b6ed3db142ed7885ae194e07de7104d971
Zhenxu Wang <mason123wp@gmail.com>
2023-02-11 20:40:28 -0500

    Added static method findRecur().

--- src/businessLogic/Recurrence.java
+++ src/businessLogic/Recurrence.java
@@ -1,13 +1,16 @@
 package businessLogic;
 
+import java.util.ArrayList;
 import java.util.Date;
+
+import persistence.LedgerItem;
 
 public class Recurrence {
 	private Date date;
 	private String frequency;
 	
 	/**
-	 * 
+	 * This constructor constructs Recurrence object. 
 	 * @param 	date is the entry billing/payment date.
 	 * @param 	frequency must be one of: "weekly", "biweekly", "monthly" and "yearly".
 	 */
@@ -47,5 +50,21 @@ public class Recurrence {
 		this.frequency = frequency;
 	}
 	
-	
+	/**
+	 * This static method returns all recurring LedgerItem objects 
+	 * in a new ArrayList from argument list.
+	 * 
+	 * @param 	<T> == <? extends LedgerItem>
+	 * @param 	list is an ArrayList of <T>
+	 * @return	all recurring LedgerItem objects.
+	 */
+	public static <T extends LedgerItem> ArrayList<T> findRecur(ArrayList<T> list) {
+		ArrayList<T> res = new ArrayList<>();
+		for (T ledger : list) {
+			if (ledger.getRecurring() != null) {
+				res.add(ledger);
+			}
+		}
+		return res;
+	}
 }
