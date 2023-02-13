57bb23caa63ee28e710582933c2844be2b2170f0
Mansteinvon <36380930+Mansteinvon@users.noreply.github.com>
2023-02-13 13:34:07 -0800

    modify constructor for recurssive income

--- src/persistence/Recurssiveincome.java
+++ src/persistence/Recurssiveincome.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Recurssiveincome extends Income {
 
-	public Recurssiveincome(Date date, double amount, String event, String note) {
+	public Recurssiveincome(String date, double amount, String event, String note) {
 		super(date, amount, event, note);
 		// TODO Auto-generated constructor stub
 	}
