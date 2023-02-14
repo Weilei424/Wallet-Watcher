b00c75e7935733d69c1615c8b5b751e42bdc8d11
Mansteinvon <36380930+Mansteinvon@users.noreply.github.com>
2023-02-14 05:58:34 -0800

    Create stockfund class

---
+++ src/persistence/Stock_Fund.java
@@ -0,0 +1,10 @@
+package persistence;
+
+public class Stock_Fund extends Investment {
+
+	protected Stock_Fund(String date, double amount, String itemName, String note) {
+		super(date, amount, itemName, note);
+		// TODO Auto-generated constructor stub
+	}
+
+}
