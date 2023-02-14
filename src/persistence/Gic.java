9aa8c15da4bd0e982cf3d5a32be842301926a12a
Mansteinvon <36380930+Mansteinvon@users.noreply.github.com>
2023-02-14 05:59:25 -0800

    "create gic class

---
+++ src/persistence/Gic.java
@@ -0,0 +1,10 @@
+package persistence;
+
+public class Gic extends Investment {
+
+	protected Gic(String date, double amount, String itemName, String note,String length) {
+		super(date, amount, itemName, note);
+		
+	}
+
+}
--- src/persistence/Immediate_Income.java
+++ src/persistence/Immediate_Income.java
@@ -6,7 +6,7 @@ public class Immediate_Income extends Income {
 
 	public Immediate_Income(String date, double amount, String event, String note) {
 		super(date, amount, event, note);
-		
+		//Store the 
 	}
 	
 
