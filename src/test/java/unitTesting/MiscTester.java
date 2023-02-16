fdad66c8611d0a0f05b20ea565d1fd25a79ee4fd
Mansteinvon <36380930+Mansteinvon@users.noreply.github.com>
2023-02-15 22:29:41 -0800

    adding test method for Misc test

--- src/test/java/unitTesting/MiscTester.java
+++ src/test/java/unitTesting/MiscTester.java
@@ -2,13 +2,24 @@ package test.java.unitTesting;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class MiscTester {
+import main.java.persistence.Misc;
 
+class MiscTester {
+Misc tfsa;
+	@BeforeEach
+	void init() {
+		
+		
+	}
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		String date ="Feb 11,2021";
+		double amount =1000;
+		tfsa = new Misc(date,amount);
+		assertEquals(tfsa.getAmount(),1000);
 	}
 
 }
