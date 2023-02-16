8611a0e00e967ef1632c3e6eb9f262ee7160abee
Tahas022 <taha.shahid70@gmail.com>
2023-02-15 22:21:18 -0500

    worked on bugs in the JTextArea

--- src/main/java/ExpensePage.java
+++ src/main/java/ExpensePage.java
@@ -12,60 +12,64 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class ExpensePage {
-	
-	//Initializing global variables
+
+	// Initializing global variables
 	JFrame mainEpFrame;
 	JPanel mainEpPanel;
-	JPanel mainPanel; //main panel to hold all other panels in the expense page form
-	//JPanel ledgerPanel;
+	JPanel mainPanel; // main panel to hold all other panels in the expense page form
+	// JPanel ledgerPanel;
 	JButton addExpense;
 	JButton removeExpense;
 	JLabel title;
 	JTextArea ledgerInfo;
 
-	
-	
 	public ExpensePage() {
 		mainEpFrame = new JFrame();
 		mainEpPanel = new JPanel();
-		
-		//Initialize main title on page, along with initializing button and layouts
+
+		// Initialize main title on page, along with initializing button and layouts
 		title = new JLabel("Expenses");
 		title.setSize(30, 30);
 		title.setFont(new Font("Tahoma", Font.BOLD, 60));
-		
+
 		addExpense = new JButton("Add New Expense");
 		addExpense.setSize(40, 40);
-		
+
 		removeExpense = new JButton("Remove Expense");
 		removeExpense.setBackground(Color.green);
 
-		//This panel holds the top elements including the title and the ability to add another button
+		// This panel holds the top elements including the title and the ability to add
+		// another button
 		mainEpPanel.setLayout(new GridLayout(1, 2));
 		mainEpPanel.add(title);
 		mainEpPanel.add(addExpense);
 		mainEpPanel.setBackground(Color.green);
-		
-		//This is the text area which shows all of the "ledger" information
-		
+
+		// This is the text area which shows all of the "ledger" information
+
 		ledgerInfo = new JTextArea();
 		ledgerInfo.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		// Disables user from being able to add any text into area as it is only for
+		// displaying the ledger
+		ledgerInfo.setEditable(false);
+
 		
-		
-		//This panel holds all other elements in the frame
+
+		// This panel holds all other elements in the frame
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new BorderLayout());
 		mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		mainPanel.add(mainEpPanel);
 		mainPanel.setBackground(Color.green);
-		//mainPanel.add(ledgerPanel);
-		
-		//This is the main frame which holds the main panel and all other elements enclosed in it
+
+		// This is the main frame which holds the main panel and all other elements
+		// enclosed in it
 		mainEpFrame.add(mainPanel, BorderLayout.NORTH);
 		mainEpFrame.add(ledgerInfo, BorderLayout.CENTER);
 		mainEpFrame.add(removeExpense, BorderLayout.SOUTH);
@@ -74,13 +78,11 @@ public class ExpensePage {
 		mainEpFrame.setSize(800, 800);
 		// expensePageFrame.pack(); // when setSize on, then remove pack
 		mainEpFrame.setVisible(true);
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		new ExpensePage();
 	}
-	
-	
 
 }
