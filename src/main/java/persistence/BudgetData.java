665488dd71c3c370b41b7e08a67014bc3c335a55
syed132 <syed132@my.yorku.ca>
2023-02-16 20:50:55 -0500

    changed budget class to not be a ledgerItem

--- src/main/java/persistence/BudgetData.java
+++ src/main/java/persistence/BudgetData.java
@@ -1,13 +1,14 @@
 package main.java.persistence;
 
-public class BudgetData extends LedgerItem {
+public class BudgetData {
 
-    public BudgetData(String date, double amount, String itemName, String note) {
-        super(date, amount, itemName, note);
+	public double budget; 
+    public BudgetData(double budget) {
+    	this.budget=budget; 
     }
 
     public double getMoneyLeft(ExpenseInputData expenseInputData) {
-        return amount - expenseInputData.cost;
+        return budget - expenseInputData.cost;
     }
 
 }
