9169c973c5cea5021bf2498de75e28ef5cf670c3
Zhenxu Wang <mason123wp@gmail.com>
2023-02-23 00:41:24 -0500

    updated project build and removed jfreechart from maven dependencies

--- src/main/java/persistence/MysqlConnection.java
+++ src/main/java/persistence/MysqlConnection.java
@@ -6,26 +6,19 @@ import java.sql.SQLException;
 
 public class MysqlConnection {
 	
-//	private static final String USERNAME = "team7";
-//	private static final String PASSWORD = "eecs2311!";
-//	private static final String CONN_STRING = "jdbc:mysql://wallet-watcher.mysql.database.azure.com:3306/mysql?useSSL=true";
-//	
-//	public static void main(String[] args) throws SQLException {
-//		
-//		Connection conn = null;
-//		
-//		try {
-//			conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
-//			System.out.println("Connected!");
-//		} catch (SQLException e) {
-//			System.err.println(e);
-//		} finally {
-//			if (conn != null) {
-//				conn.close();
-//			}
-//		}
-//		
-//		
-//	}
+	private static final String USERNAME = "team7";
+	private static final String PASSWORD = "eecs2311!";
+	private static final String CONN_STRING = "jdbc:mysql://wallet-watcher.mysql.database.azure.com:3306/mysql?useSSL=true";
+	
+	public static void main(String[] args) throws SQLException {
+		
+		try (
+				Connection conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+				) {
+			System.out.println("Connected!");
+		} catch (SQLException e) {
+			System.err.println(e);
+		} 
+	}
 	
 }
