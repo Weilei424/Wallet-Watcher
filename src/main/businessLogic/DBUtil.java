38959249b981a1d331de751cd3c815d49fa826c9
Zhenxu Wang <mason123wp@gmail.com>
2023-03-03 17:50:10 -0500

    Added feature to update ref# from DB for User object.

--- src/main/businessLogic/DBUtil.java
+++ src/main/businessLogic/DBUtil.java
@@ -20,11 +20,13 @@ public final class DBUtil {
 	
 	public static void createUser(User u) throws Exception {
 		String query = "INSERT INTO users (username, pd, salt, firstname, lastname, acctype) values (?, ?, ?, ?, ?, ?)";
+		String getRef = "SELECT * FROM users WHERE username=?";
 		ResultSet rs = null;
 		
 		try (
 				Connection conn = DBUtil.getConnection("users");
 				PreparedStatement p = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+				PreparedStatement ref = conn.prepareStatement(getRef, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 				) {
 			p.setString(1, u.getUserName());
 			p.setString(2, u.getPassword());
@@ -32,13 +34,16 @@ public final class DBUtil {
 			p.setString(3, u.getFirstName());
 			p.setString(4, u.getLastName());
 			p.setString(5, u.getType());
-			rs = p.executeQuery();
-			
+			p.executeQuery();
+			ref.setString(1, u.getUserName());
+			rs = ref.executeQuery();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			if (rs != null)
+			if (rs != null) {
+				u.setRef(rs.getInt("ref"));
 				rs.close();
+			}
 		}
 	}
 }
