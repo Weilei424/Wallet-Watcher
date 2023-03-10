ccae5693b40fcf55c3eb0bfc423c829e99c15434
Tahas022 <taha.shahid70@gmail.com>
2023-03-09 21:43:16 -0500

    added main menu to card purse page

--- src/main/java/UI/CardPursePage.java
+++ src/main/java/UI/CardPursePage.java
@@ -23,33 +23,30 @@ import persistence.DBUtil;
 import persistence.LedgerItem;
 
 public class CardPursePage implements ActionListener {
-	
+
 	public JFrame mainCpPage;
 	private JPanel mainCpPanel;
 	private JPanel mainPanel; // main panel to hold all other panels in the expense page form
 	// JPanel ledgerPanel;
 	private JButton addNewCard;
 	private JButton removeCard;
+	private JButton toMenu;
 	private JLabel title;
-	//private JTextArea cardInfo;
+	// private JTextArea cardInfo;
 	private ExpensePageForm epForm;
 	private LedgerItem tempLedgerItem;
 	private JTable cardPurseTable;
 	private JScrollPane cardScroller;
+	private navigatorPage nav;
 
 	public CardPursePage() {
-		
-		try
-		{ 
-	       	cardPurseTable = DBUtil.query("ceojeff","tag","card");
-		}
-		catch(SQLException er)
-		{ 
+
+		try {
+			cardPurseTable = DBUtil.query("ceojeff", "tag", "card");
+		} catch (SQLException er) {
 		}
 		cardScroller = new JScrollPane(cardPurseTable);
-		
-		
-		
+
 		mainCpPage = new JFrame();
 		mainCpPanel = new JPanel();
 		this.tempLedgerItem = tempLedgerItem;
@@ -63,11 +60,21 @@ public class CardPursePage implements ActionListener {
 		addNewCard.setSize(40, 40);
 		addNewCard.addActionListener(this);
 
+		toMenu = new JButton(new AbstractAction("Main Menu") {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				nav = new navigatorPage();
+				mainCpPage.dispose();
+			}
+		});
+
 		// This panel holds the top elements including the title and the ability to add
 		// another button
-		mainCpPanel.setLayout(new GridLayout(1, 2));
+		mainCpPanel.setLayout(new GridLayout(1, 3));
 		mainCpPanel.add(title);
 		mainCpPanel.add(addNewCard);
+		mainCpPanel.add(toMenu);
 		mainCpPanel.add(cardScroller);
 		mainCpPanel.setBackground(Color.green);
 
@@ -109,24 +116,23 @@ public class CardPursePage implements ActionListener {
 		// This is the main frame which holds the main panel and all other elements
 		// enclosed in it
 		mainCpPage.add(mainPanel, BorderLayout.NORTH);
-	//	mainCpPage.add(cardInfo, BorderLayout.CENTER);
+		// mainCpPage.add(cardInfo, BorderLayout.CENTER);
 		mainCpPage.add(cardScroller, BorderLayout.CENTER);
 		mainCpPage.add(removeCard, BorderLayout.SOUTH);
 		mainCpPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainCpPage.setTitle("Expenses");
-		mainCpPage.setSize(755, 1000);
+		mainCpPage.setSize(1000, 1000);
 		// expensePageFrame.pack(); // when setSize on, then remove pack
 		mainCpPage.setVisible(true);
 
 	}
 
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		new CardPursePage();
 	}
