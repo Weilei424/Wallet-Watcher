d1ff6d4edcc53b71d3516f7d7174f7e4ce260cdd
Mansteinvon <36380930+Mansteinvon@users.noreply.github.com>
2023-03-10 23:18:40 -0800

    output form test

---
+++ src/test/java/unitTesting/OutputformTest.java
@@ -0,0 +1,14 @@
+package unitTesting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OutputformTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
