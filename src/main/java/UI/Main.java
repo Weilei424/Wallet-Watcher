59ce447d70b9b4898c3a23b4ee77c66a93e53070
Mansteinvon <36380930+Mansteinvon@users.noreply.github.com>
2023-03-10 21:39:15 -0800

    change based on main branch one

--- src/main/java/UI/Main.java
+++ src/main/java/UI/Main.java
@@ -1,11 +1,11 @@
 package UI;
 
-import com.formdev.flatlaf.FlatLightLaf;
+//import com.formdev.flatlaf.FlatLightLaf;
 
 public class Main {
 
 	public static void main(String[] args) {
-		FlatLightLaf.setup();
+		//FlatLightLaf.setup();
 		MainUi login = new MainUi();
 	}
 
