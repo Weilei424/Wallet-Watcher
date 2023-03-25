00fa9d1821fad3533fd057c07e6764dab8351386
Zhenxu Wang <mason123wp@gmail.com>
2023-03-25 00:27:58 -0400

    Added update and delete pop up menu on right click on table

--- src/main/java/UI/CardPursePage.java
+++ src/main/java/UI/CardPursePage.java
@@ -2,22 +2,33 @@ package UI;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dialog;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.SQLException;
 
 import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 import DB.DBUtil;
 import persistence.LedgerItem;
@@ -40,7 +51,16 @@ public class CardPursePage implements ActionListener {
 	public JScrollPane cardScroller;
 	private navigatorPage nav;
 	private CardPursePageForm cppf;
-
+	private JPopupMenu popupMenu;
+	private JMenuItem updateMenuItem;
+	private JMenuItem deleteMenuItem;
+	private JDialog dialog;
+	private JRadioButton item;
+	private JRadioButton note;
+	private JRadioButton amount;
+	private JPanel dialogPanel;
+	private ButtonGroup buttonGroup;
+	
 	public CardPursePage() {
 
 		try {
@@ -81,6 +101,111 @@ public class CardPursePage implements ActionListener {
 		mainCpPanel.add(cardScroller);
 		mainCpPanel.setBackground(Color.green);
 
+		// pop up menu, on click for update and delete
+				this.popupMenu = new JPopupMenu();
+				this.updateMenuItem = new JMenuItem("Update");
+				this.deleteMenuItem = new JMenuItem("Delete");
+				popupMenu.add(updateMenuItem);
+				popupMenu.add(deleteMenuItem);
+				
+				cardPurseTable.addMouseListener(new MouseAdapter() {
+				    public void mousePressed(MouseEvent e) {
+				        // check if the mouse button pressed is the right button
+				        if (SwingUtilities.isRightMouseButton(e)) {
+				            // get the row index of the clicked cell
+				            int row = cardPurseTable.rowAtPoint(e.getPoint());
+				            
+				            // if the row index is valid, select the row
+				            if (row >= 0 && row < cardPurseTable.getRowCount()) {
+				            	cardPurseTable.setRowSelectionInterval(row, row);
+				            }
+				            
+				            // show the popup menu
+				            popupMenu.show(e.getComponent(), e.getX(), e.getY());
+				        }
+				    }
+				});
+
+				updateMenuItem.addActionListener(new ActionListener() {
+				    public void actionPerformed(ActionEvent e) {
+				        int row = cardPurseTable.getSelectedRow();
+				        int ref = (int) cardPurseTable.getModel().getValueAt(row, 0);
+				        
+				        if (row != -1) {
+				            // Get the value of the selected row's ID column
+				            int id = (int) cardPurseTable.getValueAt(row, 0);
+				            
+				            // Create a new dialog box to prompt the user for input
+				            dialog = new JDialog(mainCpPage, "Update Item", Dialog.ModalityType.APPLICATION_MODAL);
+				            dialog.setPreferredSize(new Dimension(500, 400));
+				            dialogPanel = new JPanel(new GridLayout(0, 1));
+				            JLabel label = new JLabel("Enter new value:");
+				            JTextField textField = new JTextField();
+				            item = new JRadioButton("Item name");
+				            note = new JRadioButton("Note");
+				            amount = new JRadioButton("Amount");
+				            buttonGroup = new ButtonGroup();
+				            buttonGroup.add(item);
+				            buttonGroup.add(note);
+				            buttonGroup.add(amount);
+				            dialogPanel.add(label);
+				            dialogPanel.add(textField);
+				            dialogPanel.add(item);
+				            dialogPanel.add(note);
+				            dialogPanel.add(amount);
+				            
+				            // Create a "Submit" button to close the dialog box
+				            JButton submitButton = new JButton("Submit");
+				            submitButton.addActionListener(new ActionListener() {
+				                public void actionPerformed(ActionEvent e) {
+				                    // Get the value entered by the user
+				                	String selection = "";
+				                    String newValue = textField.getText();
+				                    boolean option1Selected = item.isSelected();
+				                    boolean option2Selected = note.isSelected();
+				                    boolean option3Selected = amount.isSelected();
+				                    
+				                    if (option1Selected) {
+				                        selection = "item";
+				                        cardPurseTable.setValueAt(newValue, row, 1);
+				                    } else if (option2Selected) {
+				                        selection = "note";
+				                        cardPurseTable.setValueAt(newValue, row, 2);
+				                    } else if (option3Selected) {
+				                        selection = "amount";
+				                        cardPurseTable.setValueAt(newValue, row, 3);
+				                    }
+				                    DBUtil.update(User.getLoginAs(), ref, selection, newValue);
+				                    // Close the dialog box
+				                    dialog.dispose();
+				                }
+				            });
+				            dialogPanel.add(submitButton);
+				            
+				            dialog.add(dialogPanel);
+				            dialog.pack();
+				            dialog.setLocationRelativeTo(null);
+				            dialog.setVisible(true);
+				        }
+				    }
+				});
+
+				deleteMenuItem.addActionListener(new ActionListener() {
+				    public void actionPerformed(ActionEvent e) {
+				        int row = cardPurseTable.getSelectedRow();
+				        int ref = (int) cardPurseTable.getModel().getValueAt(row, 0);
+				        DBUtil.delete(User.getLoginAs(), ref);
+				        try
+						{ 
+				        	cardPurseTable = DBUtil.query(User.getLoginAs(),"tag","investment");
+						}
+						catch(SQLException er)
+						{ 
+						}
+						cardScroller = new JScrollPane(cardPurseTable);
+				    }
+				});
+		
 		// This is the text area which shows all of the "ledger" information
 
 //		cardInfo = new JTextArea();
